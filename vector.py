2c5778c6a920c4fd6c1a4a1f149ffe504518c28c
Kartikeya <kartikeya905@gmail.com>
2024-12-09 20:18:16 +0530

    File change

--- vector.py
+++ vector.py
@@ -1,11 +1,27 @@
-from initializer import *
-
+import os
+import getpass
+from langchain_ollama import ChatOllama
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import SKLearnVectorStore
 from langchain_nomic.embeddings import NomicEmbeddings
+
+
+local_llm = "llama3.2:latest"
+llm = ChatOllama(model=local_llm, temperature=0)
+llm_json_mode = ChatOllama(model=local_llm, temperature=0, format="json")
+def _set_env(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"{var}: ")
+_set_env("TAVILY_API_KEY")
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+_set_env("LANGSMITH_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "local-llama32-rag"
+
 urls=["https://www.geeksforgeeks.org/python-dictionary-get-method/",
     ]
+
 
 # Load documents
 docs = [WebBaseLoader(url).load() for url in urls]
